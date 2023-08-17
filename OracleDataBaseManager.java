0574305d2bac92a454897b8c341dfa0bd06a315a
yakupozbey <93785821+yakupozbey@users.noreply.github.com>
2023-08-18 00:34:31 +0300

    Delete OracleDataBaseManager.java

--- OracleDataBaseManager.java
+++
@@ -1,9 +0,0 @@
-package abstractDemo;
-
-public class OracleDataBaseManager extends BaseDateBaseManager{
-	@Override
-	public void getData() {
-		System.out.println("Veri getirildi : Oracle");
-	}
-
-}
